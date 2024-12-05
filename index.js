3afca7bdfce69dba8879dc36240f3b8e82a02012
unknown <martintovar6270@digitalcollege.internal>
2024-12-05 12:03:21 -0300

    let e const

---
+++ index.js
@@ -0,0 +1,20 @@
+document.write(`<h1 class="titulo">Hello World!</h1>`);
+
+let resultado = 2+2;
+document.write(`<h1 class="titulo">Quanto é 2 + 2?</h1>`);
+document.write(`<h1 class="titulo">${resultado}</h1>`);
+document.write(`<h1 class="titulo">Adicione o dobro</h1>`);
+document.write(`<h1 class="titulo">${resultado + 8}</h1>`);
+
+let nome = "Leo";
+
+document.write(`<h1>Seja bem-vindo(a) ${nome}</h1>`);
+
+let habilitado = true;
+habilitado = false;
+
+document.write(`<h1>O ${nome} ${habilitado ? 'é' : 'não é'} habilitado</h1>`)
+
+const nomeDuplo = "camelCase";
+const nome_duplo = "snake_case";
+const Pascalduplo= "PascalCase";
